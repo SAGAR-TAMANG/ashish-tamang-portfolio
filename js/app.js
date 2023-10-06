f5aad2d0137760d53595b1fa8daac50c9ba8e9b9
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-10-06 22:16:06 +0530

    updates, fixed nav

--- js/app.js
+++ js/app.js
@@ -26,6 +26,33 @@ function toggleMenu(){
   }
 }
 
+var btnMenu = document.getElementById("btn-menu");
+
+btnMenu.addEventListener("click", function(){
+  toggleMenu();
+});
+
+function toggleMenu(){
+  var nav = document.getElementById("nav");
+  var slider = document.getElementsByClassName("slider");
+  
+  // if (nav.style.display === "block") {
+  //   nav.style.display = "none";
+  // } else {
+  //   nav.style.display = "block";
+  // }
+    
+  if (nav.style.width === "50%") {
+    nav.style.width = "0%";
+    // btnMenu.style.backgroundColor = "var(--text)";
+    // btnMenu.style.color = "var(--bg)";
+  } else {
+    nav.style.width = "50%";
+    // btnMenu.style.backgroundColor = "var(--bg)";
+    // btnMenu.style.color = "var(--text)";  
+  }
+}
+
 var isButtonPressed = false;
 
 function toggleTheme(){
