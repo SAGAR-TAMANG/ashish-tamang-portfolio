80eed0cce37ef481a478111245e3ea210a8359a6
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-10-14 14:35:24 +0530

    updates

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function submitForm(event) {
   console.log('Phone:', phone);
   console.log('Message:', message);
   console.log('Terms Checked:', termsChecked);
-}
+};
 
 function toggleMenu(){
   var nav = document.getElementById("nav");
@@ -24,7 +24,7 @@ function toggleMenu(){
   } else {
     nav.style.display = "block";
   }
-}
+};
 
 var btnMenu = document.getElementById("btn-menu");
 
@@ -51,7 +51,7 @@ function toggleMenu(){
     // btnMenu.style.backgroundColor = "var(--bg)";
     // btnMenu.style.color = "var(--text)";  
   }
-}
+};
 
 var isButtonPressed = false;
 
@@ -72,16 +72,11 @@ function toggleTheme(){
       isButtonPressed = false;
     }, 10); // Adjust the time as needed
   }
-}
-
-var ashishIntroMiddle = document.getElementById("ashish-intro-middle");
-var verticalLine = document.getElementById("vertical-line");
-
-verticalLine.style.height = (ashishIntroMiddle.offsetHeight + 40) + 'px';
+};
 
 // Smooth Scrolling
 
-nav_btn = document.querySelectorAll('#nav ul a')
+// nav_btn = document.querySelectorAll('#nav ul a');
 
 // btn1.addEventListener('click', function (e) {
 //   e.preventDefault();
@@ -95,6 +90,17 @@ var currentFileName = window.location.pathname.split('/').pop();
 
 // Check if the file name is "example.html"
 if (currentFileName === "index.html") {
+
+  nav_btn = document.querySelectorAll('#nav ul a');
+
+  // CODE FOR VERTICAL LINE IN THE MIDDLE HEADER
+  var ashishIntroMiddle = document.getElementById("ashish-intro-middle");
+  var verticalLine = document.getElementById("vertical-line");
+
+  verticalLine.style.height = (ashishIntroMiddle.offsetHeight + 40) + 'px';
+
+  // Smooth Scrolling
+
   nav_btn.forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -105,20 +111,123 @@ if (currentFileName === "index.html") {
     });
   });
 
-} else {
-  
 };
 
 // POP UP BEGINS
-
-function showPopupPeople1() {
-  document.getElementById('popup-people-1').style.display = 'block';
+if (currentFileName === "people-and-leadership.html"){
+  btn1 = document.getElementById('popup-link-people-1');
+  btn2 = document.getElementById('popup-link-people-2');
+  
+  function showPopupPeople1() {
+    document.getElementById('popup-people-1').style.display = 'block';
+  }
+  
+  function closePopupPeople1() {
+    document.getElementById('popup-people-1').style.display = 'none';
+  }
+  
+  btn1.addEventListener('click', function(event) { 
+    showPopupPeople1();
+  });
+  
+  function showPopupPeople2() {
+    document.getElementById('popup-people-2').style.display = 'block';
+  }
+  
+  function closePopupPeople2() {
+    document.getElementById('popup-people-2').style.display = 'none';
+  }
+  
+  btn2.addEventListener('click', function() { 
+    showPopupPeople2();
+  });
 }
 
-function closePopupPeople1() {
-  document.getElementById('popup-people-1').style.display = 'none';
-}
+// Starts the Carousel / Image Slider
 
-document.getElementById('popup-link-people-1').addEventListener('click', function(event) { 
-  console.log("CLICKED");
-});
+if (currentFileName === "project.html") {
+  const wrapper = document.querySelector(".wrapper");
+  const carousel = document.querySelector(".carousel");
+  const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+  const arrowBtns = document.querySelectorAll(".wrapper i");
+  const carouselChildrens = [...carousel.children];
+
+  let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
+
+  // Get the number of cards that can fit in the carousel at once
+  let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+  // Insert copies of the last few cards to beginning of carousel for infinite scrolling
+  carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+      carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+  });
+
+  // Insert copies of the first few cards to end of carousel for infinite scrolling
+  carouselChildrens.slice(0, cardPerView).forEach(card => {
+      carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+  });
+
+  // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
+  carousel.classList.add("no-transition");
+  carousel.scrollLeft = carousel.offsetWidth;
+  carousel.classList.remove("no-transition");
+
+  // Add event listeners for the arrow buttons to scroll the carousel left and right
+  arrowBtns.forEach(btn => {
+      btn.addEventListener("click", () => {
+          carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+      });
+  });
+
+  const dragStart = (e) => {
+      isDragging = true;
+      carousel.classList.add("dragging");
+      // Records the initial cursor and scroll position of the carousel
+      startX = e.pageX;
+      startScrollLeft = carousel.scrollLeft;
+  }
+
+  const dragging = (e) => {
+      if(!isDragging) return; // if isDragging is false return from here
+      // Updates the scroll position of the carousel based on the cursor movement
+      carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+  }
+
+  const dragStop = () => {
+      isDragging = false;
+      carousel.classList.remove("dragging");
+  }
+
+  const infiniteScroll = () => {
+      // If the carousel is at the beginning, scroll to the end
+      if(carousel.scrollLeft === 0) {
+          carousel.classList.add("no-transition");
+          carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+          carousel.classList.remove("no-transition");
+      }
+      // If the carousel is at the end, scroll to the beginning
+      else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
+          carousel.classList.add("no-transition");
+          carousel.scrollLeft = carousel.offsetWidth;
+          carousel.classList.remove("no-transition");
+      }
+
+      // Clear existing timeout & start autoplay if mouse is not hovering over carousel
+      clearTimeout(timeoutId);
+      if(!wrapper.matches(":hover")) autoPlay();
+  }
+
+  const autoPlay = () => {
+      if(window.innerWidth < 800 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
+      // Autoplay the carousel after every 2500 ms
+      timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
+  }
+  autoPlay();
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("mousemove", dragging);
+  document.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("scroll", infiniteScroll);
+  wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+  wrapper.addEventListener("mouseleave", autoPlay);
+};
